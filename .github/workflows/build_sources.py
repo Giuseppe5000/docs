4c79aeb7f01b189296063997eb0a98a288f46c42
Giuseppe Tutino <32134603+Giuseppe5000@users.noreply.github.com>
2024-11-07 20:30:28 +0100

    Update build_sources.py

--- .github/workflows/build_sources.py
+++ .github/workflows/build_sources.py
@@ -27,7 +27,7 @@ typst_sources_to_compile = [el for el in sources_to_compile if ".typ" in el]
 # Make dirs
 build_dir = "{}/build/".format(repo_dir)
 for source in tex_sources_to_compile+typst_sources_to_compile:
-    os.makedirs(os.path.dirname(source))
+    os.makedirs(os.path.dirname(source), exist_ok = True)
 #if os.path.exists(build_dir):
 #    shutil.rmtree(build_dir)
 #os.makedirs("{}candidatura/documenti_interni/verbali".format(build_dir))
