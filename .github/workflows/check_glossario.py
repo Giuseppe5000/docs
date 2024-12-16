a93b6bbefb79328391a1e4861eb96f5f2c3841c7
Giuseppe Tutino <32134603+Giuseppe5000@users.noreply.github.com>
2024-12-16 17:13:24 +0100

    Update check_glossario.py

--- .github/workflows/check_glossario.py
+++ .github/workflows/check_glossario.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     for source in sources_to_check:
         source_text = get_file_text(source)
         source_text = filter_file(source, source_text)
-        fail_found = check(source, source_text, termini, termini_contenuti) || fail_found
+        fail_found = check(source, source_text, termini, termini_contenuti) | fail_found
 
     # Ritorna codice di uscita 1 se ci sono fallimenti
     if fail_found:
