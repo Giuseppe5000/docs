ba565d502ce430767cc9d506aefb5a500fd406e6
Giuseppe Tutino <32134603+Giuseppe5000@users.noreply.github.com>
2024-12-16 17:22:46 +0100

    Update check_glossario.py

--- .github/workflows/check_glossario.py
+++ .github/workflows/check_glossario.py
@@ -1,7 +1,11 @@
 import re, glob, os, sys
 
-REPO_DIR       = os.environ['GITHUB_WORKSPACE'] + '/RTB'
-FILE_GLOSSARIO = REPO_DIR + '/documenti_interni/glossario/glossario.typ'
+REPO_DIR = '../../'
+FILE_GLOSSARIO = REPO_DIR + 'RTB/documenti_interni/glossario/glossario.typ'
+
+if 'GITHUB_WORKSPACE' in os.environ:
+    REPO_DIR = os.environ['GITHUB_WORKSPACE'] + '/RTB'
+    FILE_GLOSSARIO = REPO_DIR + '/documenti_interni/glossario/glossario.typ'
 
 
 def get_file_text(f):
